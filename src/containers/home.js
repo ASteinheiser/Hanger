32e4e35d51f3cf425e9888bccccc4bc4efe67d0b
Andrew Steinheiser <me@iamandrew.io>
2018-06-05 22:03:56 -0700

    added react-native-splash-screen

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,8 +1,14 @@
 import React          from 'react';
 import { View, Text } from 'react-native';
 import styled         from 'styled-components/native';
+import SplashScreen   from 'react-native-splash-screen';
 
 export default class Home extends React.Component {
+
+  componentWillMount() {
+    setTimeout(() => { SplashScreen.hide(); }, 1000);
+  }
+
   render() {
     return (
       <Flex>
