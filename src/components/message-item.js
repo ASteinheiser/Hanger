b693f8f9a302258b2da0129aa0d18f648d03652f
Andrew Steinheiser <me@iamandrew.io>
2018-07-05 20:50:54 -0700

    added messageItem and messages to message page

---
+++ src/components/message-item.js
@@ -0,0 +1,60 @@
+import React          from 'react';
+import { Text, View } from 'react-native';
+import { Avatar }     from 'react-native-material-ui';
+import styled         from 'styled-components';
+
+import theme from '../theme.js';
+
+export default class MessageItem extends React.Component {
+  render() {
+    return (
+      <Container color={theme.palette.disabledColor}>
+
+        <ImageContainer>
+          <Avatar icon='person' iconColor='gray' size={40} iconSize={30} />
+        </ImageContainer>
+
+        <FlexColumn>
+
+          <ColoredText color={theme.palette.secondaryTextColor}>
+            { this.props.user }
+            <ColoredText color={theme.palette.disabledColor}>
+              { '  |  ' + this.props.timestamp }
+            </ColoredText>
+          </ColoredText>
+
+          <ColoredText color={theme.palette.primaryTextColor}>
+            { this.props.text }
+          </ColoredText>
+
+        </FlexColumn>
+
+      </Container>
+    )
+  }
+}
+
+const ColoredText = styled.Text`
+  font-size: 14px;
+  color: ${props => props.color};
+  padding: 3px 0 0 7px;
+`
+
+const Container = styled.View`
+  flex: 1;
+  flex-direction: row;
+
+  padding: 15px;
+`
+
+const FlexColumn = styled.View`
+  flex: 1;
+  flex-direction: column;
+
+  align-self: flex-start;
+`
+
+const ImageContainer = styled.View`
+  flex-basis: 50px;
+  justify-content: center;
+`
--- src/containers/messages.js
+++ src/containers/messages.js
@@ -1,23 +1,34 @@
 import React                from 'react';
 import { View, ScrollView } from 'react-native';
 import styled               from 'styled-components';
+import _map                 from 'lodash.map';
 
 import HeaderText         from '../components/header-text.js';
-import IconWithText       from '../components/icon-with-text.js';
+import MessageItem        from '../components/message-item.js';
 import TopNavigationBasic from '../components/top-navigation-basic.js';
 import theme              from '../theme.js';
 
+import SAMPLE_MESSAGES from '../../assets/messages.json';
+
 export default class Messages extends React.Component {
   render() {
+    const Messages = _map(SAMPLE_MESSAGES, message =>
+      <MessageItem
+        key={message.id}
+        user={message.user}
+        text={message.text}
+        timestamp={message.timestamp} />
+    );
+
     return (
       <Flex>
         <TopNavigationBasic navigation={this.props.navigation} />
 
         <Container color={theme.palette.canvasColor}>
           <HeaderText text='Messages' />
-          <IconWithText
-            icon='message'
-            text='Read your messages here' />
+
+          { Messages }
+
         </Container>
       </Flex>
     )
