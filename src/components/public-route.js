5aebcc2c09fb0a0c77bb76995fb5813948b6108f
Andrew Steinheiser <me@iamandrew.io>
2018-08-01 01:37:49 -0700

    added react native indicators and added checking credentials page on public route load

--- src/components/public-route.js
+++ src/components/public-route.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { withRouter }       from 'react-router-native';
 import { Auth }             from 'aws-amplify';
+import { withRouter }       from 'react-router-native';
+import { DotIndicator }     from 'react-native-indicators';
+import styled               from 'styled-components/native';
+
+import theme from '../theme.js';
 
 class PublicRoute extends Component {
   constructor(props) {
@@ -48,10 +52,44 @@ class PublicRoute extends Component {
   render() {
     let Comp = this.props.component;
 
+    if(!this.state.authenticationComplete) {
+      return(
+        <StyledView color={theme.palette.canvasColor}>
+          <Container>
+
+            <StyledText color={theme.palette.primaryColor}>
+              {'Checking Credentials...'}
+            </StyledText>
+
+            <DotIndicator color={theme.palette.primaryColor}/>
+
+          </Container>
+        </StyledView>
+      )
+    }
+
     return (
       <Comp {...this.props} />
     );
   }
 }
+
+const StyledView = styled.View`
+  background: ${props => props.color};
+
+  height: 100%;
+`
+
+const Container = styled.View`
+  margin: 50px auto;
+`
+
+const StyledText = styled.Text`
+  font-size: 28px;
+
+  color: ${props => props.color};
+
+  margin-bottom: 50px;
+`
 
 export default withRouter(PublicRoute);
