e1eebdca062a87cbb395e92fa04c26a8acdb4f33
Andrew Steinheiser <me@iamandrew.io>
2018-08-12 15:51:39 -0700

    make feed posts look better

--- src/components/feed-post.js
+++ src/components/feed-post.js
@@ -1,6 +1,6 @@
 import React                 from 'react';
 import { View, Text, Image } from 'react-native';
-import { Card }              from 'react-native-material-ui';
+import { Avatar }            from 'react-native-material-ui';
 import styled                from 'styled-components/native';
 
 import theme from '../theme.js';
@@ -8,37 +8,65 @@ import theme from '../theme.js';
 export default class FeedPost extends React.Component {
   render() {
     return (
-      <Margin>
-        <Card>
+      <PostContainer>
+        <UserInfoContainer>
+          <Touchable onPress={() => {console.log('profile click!')}}>
+            <Avatar icon='person' iconColor='gray' size={40} iconSize={30} />
+          </Touchable>
 
-          <StyledImage source={{ uri: this.props.image }} alt='feed_post' />
+          <Touchable onPress={() => {console.log('profile click!')}}>
+            <UsernameText color={theme.palette.primaryColor}>
+              { 'John Username' }
+            </UsernameText>
+          </Touchable>
+        </UserInfoContainer>
 
-          <StyledText color={theme.palette.primaryTextColor}>
-            { this.props.title }
-          </StyledText>
+        <StyledImage source={{ uri: this.props.image }} alt='feed_post' />
 
-        </Card>
-      </Margin>
+        <StyledText color={theme.palette.primaryTextColor}>
+          { this.props.title }
+        </StyledText>
+      </PostContainer>
     )
   }
 }
 
-const Margin = styled.View`
-  margin: 15px 15px 0 15px;
+const PostContainer = styled.View`
+  margin-top: 15px;
+  border-bottom-width: 1px;
+  border-bottom-color: #EEEEEE;
 `
 
 const StyledText = styled.Text`
-  font-size: 20px;
+  font-size: 16px;
   color: ${props => props.color};
 
   margin: 15px;
-  padding-top: 15px;
-
-  border-top-color: ${props => props.color};
-  border-top-width: 1px;
 `
 
 const StyledImage = styled.Image`
   height: 400px;
   width: 100%;
+`
+
+const UserInfoContainer = styled.View`
+  padding-bottom: 15px;
+  padding-left: 25px;
+
+  display: flex;
+  flex-direction: row;
+`
+
+const UsernameText = styled.Text`
+  font-size: 18px;
+  color: ${props => props.color};
+
+  padding-left: 15px;
+  text-align: center;
+
+  margin: auto 0;
+`
+
+const Touchable = styled.TouchableOpacity`
+  z-index: 101;
 `
