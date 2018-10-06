a2854de41203c052336c7383cfefcf20fc8fb78c
Andrew Steinheiser <me@iamandrew.io>
2018-10-06 16:46:14 -0700

    fleshed out more of the post page, should basically be working

--- src/containers/post.js
+++ src/containers/post.js
@@ -1,13 +1,130 @@
 import React, { Component } from 'react';
+import { API, storage }     from 'aws-amplify';
 import { withRouter }       from 'react-router-native';
 import ScaledImage          from 'react-native-scalable-image';
+import { DotIndicator }     from 'react-native-indicators';
 import styled               from 'styled-components/native';
 
-import TopNavigation       from '../components/top-navigation.js';
-import theme               from '../theme.js';
+import Alert             from '../components/alert.js';
+import Button            from '../components/button.js';
+import Input             from '../components/input.js';
+import TopNavigation     from '../components/top-navigation.js';
+import theme             from '../theme.js';
+import { uuidv4 }        from '../functions/uuid-v4.js';
+import { validateForm }  from '../functions/validate-form.js';
+import { validateField } from '../functions/validate-field.js';
 
 class Post extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: { value: '', valid: true },
+      alertMessage: '',
+      imageURI: '',
+      loading: false
+    };
+
+    this.handleProfileUpload = this.handleProfileUpload.bind(this);
+  }
+
+  onChange(field, e) {
+    if(e.nativeEvent) {
+      let { value, valid } = validateField(field, e.nativeEvent.text, this.state);
+
+      this.setState({
+        [field]: { value, valid }
+      });
+    }
+  }
+
+  handleSubmit() {
+    const { description } = this.state;
+    const formObject = { description };
+
+    const { formValid, emptyFields } = validateForm(formObject);
+
+    if (formValid) {
+      this.setState({ loading: true }, () => {
+        this.clearAlert();
+
+        this.handleProfileUpload()
+          .then(imageUrl => {
+            let params = {
+              body: {
+                description: description.value,
+                image: imageUrl
+              }
+            };
+
+            API.post('HangerAPI', '/v1/post', params)
+              .then(response => {
+                this.setState({ loading: false });
+                this.props.history.replace('/home');
+              })
+              .catch(err => {
+                console.error(err);
+                this.setState({ loading: false, alertMessage: err.message });
+              });
+          })
+          .catch(err => {
+            console.error(err);
+            this.setState({ loading: false, alertMessage: 'Error Uploading Profile Picture' });
+          });
+      });
+    } else {
+      emptyFields.forEach(fieldName => {
+        this.setState({[fieldName]: {value: '', valid: false}});
+      });
+    }
+  }
+
+  clearAlert() {
+    this.setState({ alertMessage: '' });
+  }
+
+  handleProfileUpload() {
+    return new Promise((resolve, reject) => {
+      const { imageURI } = this.state;
+
+      fetch(imageURI)
+        .then(response => {
+          response.blob()
+            .then(blob => {
+              let fileName = `${uuidv4()}.jpeg`;
+              Storage.put(fileName, blob, {
+                level: 'protected',
+                contentType: 'image/jpeg'
+              })
+                .then(result => {
+                  Storage.get(fileName, {
+                    level: 'protected',
+                    identityId: this.props.user.id
+                  })
+                    .then(result => {
+                      let profileImage = result.split('?')[0];
+
+                      resolve(profileImage);
+                    })
+                    .catch(err => {
+                      reject(err);
+                    });
+                })
+                .catch(err => {
+                  reject(err);
+                });
+            })
+            .catch(err => {
+              reject(err);
+            });
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  }
+
+  componentWillMount() {
     const { search } = this.props.location;
     let imageURI = '';
     // make sure that we have the local image uri in the query
@@ -16,12 +133,17 @@ class Post extends Component {
       // we have the uri
       if(query.length > 1) {
         imageURI = query[1];
+        this.setState({ imageURI });
       } else {
         this.props.history.push('/home');
       }
     } else {
       this.props.history.push('/home');
     }
+  }
+
+  render() {
+    const { imageURI, alertMessage, loading, description } = this.state;
 
     return(
       <Height>
@@ -32,9 +154,36 @@ class Post extends Component {
           <Container color={theme.palette.canvasColor}>
             <Centered>
               <ScaledImage
-                height={300}
+                height={400}
                 source={{uri: imageURI}} />
             </Centered>
+
+            <Alert message={alertMessage} />
+
+            <InputMargin>
+              <Input accent
+                onChange={this.onChange.bind(this, 'description')}
+                containerStyle={{ paddingLeft: 20, paddingRight: 20 }}
+                label={'Description'}
+                value={description.value}
+                error={!description.valid ? 'Enter a description' : ''}
+                />
+            </InputMargin>
+
+            <TopMargin>
+              {
+                loading ?
+                  <DotIndicator size={18} count={3} color={theme.palette.primaryColor}/>
+                  :
+                  <Button
+                    accent
+                    icon="add"
+                    text="CREATE POST"
+                    disabled={loading}
+                    onPress={this.handleSubmit.bind(this)} />
+              }
+            </TopMargin>
+
           </Container>
         </TopNavigation>
       </Height>
@@ -55,6 +204,15 @@ const Centered = styled.View`
   flex: 1;
   flex-direction: column;
   align-items: center;
+  padding: 50px 0 25px;
+`
+
+const TopMargin = styled.View`
+  margin: 30px 20% 20px 20%;
+`
+
+const InputMargin = styled.View`
+  margin: 0 5%;
 `
 
 export default withRouter(Post);
