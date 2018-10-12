77ae4dcf41b609ffcfa4751b3710b9bcb5fb0ef7
Andrew Steinheiser <me@iamandrew.io>
2018-10-12 10:38:28 -0700

    better looking active state for linked social media accounts

--- src/components/social-media-accounts.js
+++ src/components/social-media-accounts.js
@@ -11,6 +11,7 @@ import Pinterest      from '../../assets/icons/pinterest.png';
 import PinterestColor from '../../assets/icons/pinterest-color.png';
 import Twitter        from '../../assets/icons/twitter.png';
 import TwitterColor   from '../../assets/icons/twitter-color.png';
+import theme          from '../theme.js';
 
 export default class SocialMediaAccounts extends Component {
   render() {
@@ -18,20 +19,36 @@ export default class SocialMediaAccounts extends Component {
 
     return(
       <SocialContainer>
-        <SocialMediaLogo source={user.facebook ? FacebookColor : Facebook} />
-        <SocialMediaLogo source={Instagram} />
-        <SocialMediaLogo source={LinkedIn} />
-        <SocialMediaLogo source={Pinterest} />
-        <SocialMediaLogo source={Twitter} />
+
+        <ActiveBg active={user.facebook} color={theme.palette.disabledColor}>
+          <SocialMediaLogo source={user.facebook ? FacebookColor : Facebook} />
+        </ActiveBg>
+
+        <ActiveBg active={false} color={theme.palette.disabledColor}>
+          <SocialMediaLogo source={Instagram} />
+        </ActiveBg>
+
+        <ActiveBg active={false} color={theme.palette.disabledColor}>
+          <SocialMediaLogo source={LinkedIn} />
+        </ActiveBg>
+
+        <ActiveBg active={false} color={theme.palette.disabledColor}>
+          <SocialMediaLogo source={Pinterest} />
+        </ActiveBg>
+
+        <ActiveBg active={false} color={theme.palette.disabledColor}>
+          <SocialMediaLogo source={Twitter} />
+        </ActiveBg>
+
       </SocialContainer>
     );
   }
 }
 
 const SocialContainer = styled.View`
-  display: flex;
   flex: 1;
   flex-direction: row;
+  justify-content: space-around;
 
   padding: 20px;
 `
@@ -39,6 +56,15 @@ const SocialContainer = styled.View`
 const SocialMediaLogo = styled.Image`
   width: 35px;
   height: 35px;
+`
 
-  margin: 0 auto;
+const ActiveBg = styled.View`
+  width: 40px;
+  height: 40px;
+
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 20px;
+  background: ${props => props.active ? props.color : 'white'};
 `
